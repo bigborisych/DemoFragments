d977a6fe35417f8d774a01a1bdf9cd661e1a5bad
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-03 13:42:03 +0300

    Added class Item, declared item fields on RecyclerView

---
+++ app/src/main/java/com/vladborisov/demofragments/Item.java
@@ -0,0 +1,11 @@
+package com.vladborisov.demofragments;
+
+public class Item {
+    public final String name;
+    public final int count;
+
+    public Item(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+}
