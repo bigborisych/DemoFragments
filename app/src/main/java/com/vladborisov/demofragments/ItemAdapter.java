4ead277ee94a7c2f76519ac735e09e987350ac03
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-03 13:54:43 +0300

    Added class ItemAdapter, adapter Class on RecyclerView

---
+++ app/src/main/java/com/vladborisov/demofragments/ItemAdapter.java
@@ -0,0 +1,49 @@
+package com.vladborisov.demofragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    private final List<Item> items;
+
+    public ItemAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+        holder.itemView.setTag(item);
+    }
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    static final class ItemViewHolder extends RecyclerView.ViewHolder{
+        private final TextView textTextView;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textTextView = itemView.findViewById(R.id.list_item_text);
+        }
+        private void bind(@NonNull Item item){
+            String text = item.name + " " + item.count;
+            textTextView.setText(text);
+        }
+    }
+}
