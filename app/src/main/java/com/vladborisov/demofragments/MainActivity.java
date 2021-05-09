3f258bf2e2dfad6736d175631dfd252481369733
Borisov Vlad <borisov.vlad27@gmail.com>
2021-05-09 17:36:15 +0300

    Added fragment two and three, add coordinator layout

--- app/src/main/java/com/vladborisov/demofragments/MainActivity.java
+++ app/src/main/java/com/vladborisov/demofragments/MainActivity.java
@@ -1,8 +1,13 @@
 package com.vladborisov.demofragments;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.ItemClickListener {
     public static final int COUNT_ITEM_GEN = 20;
@@ -17,8 +22,27 @@ public class MainActivity extends AppCompatActivity implements ListFragment.Item
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.activity_main_fragment_container, listFragment, ListFragment.TAG)
-                .addToBackStack(null)
                 .commit();
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            FragmentTransaction fragmentManager = getSupportFragmentManager().beginTransaction();
+            if (id == R.id.fragment_two){
+                FragmentTwo fragmentTwo = new FragmentTwo();
+                fragmentManager.replace(R.id.activity_main_fragment_container, fragmentTwo, "FRAGMENT TWO").addToBackStack(null).commit();
+            }else if(id == R.id.fragment_three){
+                FragmentThree fragmentThree = new FragmentThree();
+                fragmentManager.replace(R.id.activity_main_fragment_container, fragmentThree, "FRAGMENT TWO").addToBackStack(null).commit();
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
     }
 
     @Override
